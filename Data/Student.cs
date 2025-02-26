b1651a980b5f7f8b83084d5f0b5ef72e6b2901dc
DFBlok <s219392862@mandela.ac.za>
2025-02-26 18:54:56 +0200

    Model file

---
+++ Data/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementApp.Data
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
+        [Display(Name="Full Name")]
+        public string Name { get; set; }
+        [Range(1, 100)]
+        public int Age { get; set; }
+        [Required]
+
+        public string Course { get; set; }
+
+    }
+}
