065bddddbea4caebdbe193d09f55813264e6ead7
DFBlok <s219392862@mandela.ac.za>
2025-02-27 10:48:23 +0200

    Updated the validation

--- Data/Student.cs
+++ Data/Student.cs
@@ -6,13 +6,14 @@ namespace StudentManagementApp.Data
     {
         [Key]
         public int ID { get; set; }
-        [Required(ErrorMessage = "Name is Required")]
+        [Required(ErrorMessage = "Name is required")]
         [Display(Name="Full Name")]
         public string Name { get; set; }
-        [Range(1, 100)]
+        [Required(ErrorMessage = "Age is required")]
+        [Range(1, 100, ErrorMessage = "Age must be between 1 and 100")]
         public int Age { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Course is required")]
+        [StringLength(50, ErrorMessage = "Course name cannot exceed 50 characters")]
         public string Course { get; set; }
 
     }
